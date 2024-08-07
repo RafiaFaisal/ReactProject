a228bdc28e17b90e8227394c3bef1d097436a0bb
rafiafaisal1304 <86980046+RafiaFaisal@users.noreply.github.com>
2024-08-07 15:16:02 +0500

    added files

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,26 @@
 module.exports = {
   preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(js|jsx)$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testMatch: [
+    '**/__tests__/**/*.test.(ts|tsx)',
+    '**/test/**/*.test.(ts|tsx)',
+    '**/src/**/*.test.(ts|tsx)'
+  ],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+  setupFiles: [
+    './node_modules/react-native-gesture-handler/jestSetup.js',
+    '<rootDir>/jest/setup.js',
+  ],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|@testing-library|@reduxjs/toolkit|redux-mock-store)',
+  ],
 };
--- src/components/ProductDetail/ProductDetail.container.tsx
+++ src/components/ProductDetail/ProductDetail.container.tsx
@@ -7,27 +7,22 @@ import { fetchProductDetail } from '../../store/ProductDetail/productDetail.thun
 import { RootState } from '../../store/store';
 import ProductDetailView from './ProductDetail.view';
 
-// Type Definitions for Navigation Props
 type ProductDetailScreenNavigationProp = StackNavigationProp<RootStackParamList, 'ProductDetail'>;
 type ProductDetailScreenRouteProp = RouteProp<RootStackParamList, 'ProductDetail'>;
 
-// Props Type Definition
 type Props = {
   navigation: ProductDetailScreenNavigationProp;
   route: ProductDetailScreenRouteProp;
 };
 
-// ProductDetailContainer Component
 const ProductDetailContainer: React.FC<Props> = ({ route }) => {
   const dispatch = useDispatch();
   const { product, loading, error } = useSelector((state: RootState) => state.productDetail);
 
-  // UseEffect to Fetch Product Detail
   useEffect(() => {
-    dispatch(fetchProductDetail(route.params.id) as any); // Explicitly type the dispatch call
+    dispatch(fetchProductDetail(route.params.id) as any); 
   }, [dispatch, route.params.id]);
 
-  // Render the ProductDetailView Component
   return <ProductDetailView product={product} loading={loading} error={error} />;
 };
 
